a84146368464fde0690563982ede3f66e095fd68
guilhermelim <guilhermem.lima@outlook.com>
2024-03-19 01:10:21 -0300

    Adicionado Política de Privacidade e Termos de Uso

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,269 @@
+import React from "react";
+import { AppBar, Tab, Tabs, Typography, Box } from "@mui/material";
+
+const PoliticaPrivacidade = () => {
+  return (
+    <Box>
+      <Typography variant="h4" gutterBottom>
+        Política de Privacidade
+      </Typography>
+      <Typography paragraph>
+        A sua privacidade é importante para nós. É política do Genap respeitar a
+        sua privacidade em relação a qualquer informação sua que possamos
+        coletar no site Genap, e outros sites que possuímos e operamos.
+      </Typography>
+      <Typography paragraph>
+        Solicitamos informações pessoais apenas quando realmente precisamos
+        delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais,
+        com o seu conhecimento e consentimento. Também informamos por que
+        estamos coletando e como será usado.
+      </Typography>
+      <Typography paragraph>
+        Apenas retemos as informações coletadas pelo tempo necessário para
+        fornecer o serviço solicitado. Quando armazenamos dados, protegemos
+        dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos,
+        bem como acesso, divulgação, cópia, uso ou modificação não autorizados.
+      </Typography>
+      <Typography paragraph>
+        Não compartilhamos informações de identificação pessoal publicamente ou
+        com terceiros, exceto quando exigido por lei.
+      </Typography>
+      <Typography paragraph>
+        O nosso site pode ter links para sites externos que não são operados por
+        nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas
+        desses sites e não podemos aceitar responsabilidade por suas respectivas
+        políticas de privacidade.
+      </Typography>
+      <Typography paragraph>
+        Você é livre para recusar a nossa solicitação de informações pessoais,
+        entendendo que talvez não possamos fornecer alguns dos serviços
+        desejados.
+      </Typography>
+      <Typography paragraph>
+        O uso continuado de nosso site será considerado como aceitação de nossas
+        práticas em torno de privacidade e informações pessoais. Se você tiver
+        alguma dúvida sobre como lidamos com dados do usuário e informações
+        pessoais, entre em contato conosco.
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        Serviço Google AdSense
+      </Typography>
+      <Typography paragraph>
+        O serviço Google AdSense que usamos para veicular publicidade usa um
+        cookie DoubleClick para veicular anúncios mais relevantes em toda a Web
+        e limitar o número de vezes que um determinado anúncio é exibido para
+        você. Para mais informações sobre o Google AdSense, consulte as FAQs
+        oficiais sobre privacidade do Google AdSense.
+      </Typography>
+      <Typography paragraph>
+        Utilizamos anúncios para compensar os custos de funcionamento deste site
+        e fornecer financiamento para futuros desenvolvimentos. Os cookies de
+        publicidade comportamental usados ​​por este site foram projetados para
+        garantir que você forneça os anúncios mais relevantes sempre que
+        possível, rastreando anonimamente seus interesses e apresentando coisas
+        semelhantes que possam ser do seu interesse.
+      </Typography>
+      <Typography paragraph>
+        Vários parceiros anunciam em nosso nome e os cookies de rastreamento de
+        afiliados simplesmente nos permitem ver se nossos clientes acessaram o
+        site através de um dos sites de nossos parceiros, para que possamos
+        creditá-los adequadamente e, quando aplicável, permitir que nossos
+        parceiros afiliados ofereçam qualquer promoção que pode fornecê-lo para
+        fazer uma compra.
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        Compromisso do Usuário
+      </Typography>
+      <Typography paragraph>
+        O usuário se compromete a fazer uso adequado dos conteúdos e da
+        informação que o Genap oferece no site e com caráter enunciativo, mas
+        não limitativo:
+      </Typography>
+      <Typography>
+        <ol>
+          <li>
+            <Typography>
+              Não se envolver em atividades que sejam ilegais ou contrárias à
+              boa fé a à ordem pública;
+            </Typography>
+          </li>
+          <li>
+            <Typography>
+              Não difundir propaganda ou conteúdo de natureza racista,
+              xenofóbica, Bet Nacional ou azar, qualquer tipo de pornografia
+              ilegal, de apologia ao terrorismo ou contra os direitos humanos;
+            </Typography>
+          </li>
+          <li>
+            <Typography>
+              Não causar danos aos sistemas físicos (hardwares) e lógicos
+              (softwares) do Genap, de seus fornecedores ou terceiros, para
+              introduzir ou disseminar vírus informáticos ou quaisquer outros
+              sistemas de hardware ou software que sejam capazes de causar danos
+              anteriormente mencionados.
+            </Typography>
+          </li>
+        </ol>
+      </Typography>
+      <Typography paragraph>
+        Esperemos que esteja esclarecido e, como mencionado anteriormente, se
+        houver algo que você não tem certeza se precisa ou não, geralmente é
+        mais seguro deixar os cookies ativados, caso interaja com um dos
+        recursos que você usa em nosso site.
+      </Typography>
+      <Typography paragraph>
+        Esta política é efetiva a partir de 19 March 2024 03:51
+      </Typography>
+    </Box>
+  );
+};
+
+const TermosServico = () => {
+  return (
+    <Box>
+      <Typography variant="h4" gutterBottom>
+        Termos de Serviço
+      </Typography>
+      <Typography paragraph>
+        Ao acessar ao site Genap, concorda em cumprir estes termos de serviço,
+        todas as leis e regulamentos aplicáveis ​​e concorda que é responsável
+        pelo cumprimento de todas as leis locais aplicáveis. Se você não
+        concordar com algum desses termos, está proibido de usar ou acessar este
+        site. Os materiais contidos neste site são protegidos pelas leis de
+        direitos autorais e marcas comerciais aplicáveis.
+      </Typography>
+      <Typography paragraph>
+        É concedida permissão para baixar temporariamente uma cópia dos
+        materiais (informações ou software) no site Genap, apenas para
+        visualização transitória pessoal e não comercial. Esta é a concessão de
+        uma licença, não uma transferência de título e, sob esta licença, você
+        não pode:
+      </Typography>
+      <Typography component="div">
+        <ol>
+          <li>
+            <Typography>modificar ou copiar os materiais;</Typography>
+          </li>
+          <li>
+            <Typography>
+              usar os materiais para qualquer finalidade comercial ou para
+              exibição pública (comercial ou não comercial);
+            </Typography>
+          </li>
+          <li>
+            <Typography>
+              tentar descompilar ou fazer engenharia reversa de qualquer
+              software contido no site Genap;
+            </Typography>
+          </li>
+          <li>
+            <Typography>
+              remover quaisquer direitos autorais ou outras notações de
+              propriedade dos materiais; ou
+            </Typography>
+          </li>
+          <li>
+            <Typography>
+              transferir os materiais para outra pessoa ou espelhe os materiais
+              em qualquer outro servidor.
+            </Typography>
+          </li>
+        </ol>
+      </Typography>
+      <Typography paragraph>
+        Esta licença será automaticamente rescindida se você violar alguma
+        dessas restrições e poderá ser rescindida por Genap a qualquer momento.
+        Ao encerrar a visualização desses materiais ou após o término desta
+        licença, você deve apagar todos os materiais baixados em sua posse, seja
+        em formato eletrônico ou impresso.
+      </Typography>
+      <Typography paragraph>
+        Os materiais no site da Genap são fornecidos como estão. Genap não
+        oferece garantias, expressas ou implícitas, e, por este meio, isenta e
+        nega todas as outras garantias, incluindo, sem limitação, garantias
+        implícitas ou condições de comercialização, adequação a um fim
+        específico ou não violação de propriedade intelectual ou outra violação
+        de direitos. Além disso, o Genap não garante ou faz qualquer
+        representação relativa à precisão, aos resultados prováveis ou à
+        confiabilidade do uso dos materiais em seu site ou de outra forma
+        relacionado a esses materiais ou em sites vinculados a este site.
+      </Typography>
+      <Typography paragraph>
+        Em nenhum caso o Genap ou seus fornecedores serão responsáveis por
+        quaisquer danos (incluindo, sem limitação, danos por perda de dados ou
+        lucro ou devido a interrupção dos negócios) decorrentes do uso ou da
+        incapacidade de usar os materiais em Genap, mesmo que Genap ou um
+        representante autorizado da Genap tenha sido notificado oralmente ou por
+        escrito da possibilidade de tais danos. Como algumas jurisdições não
+        permitem limitações em garantias implícitas, ou limitações de
+        responsabilidade por danos conseqüentes ou incidentais, essas limitações
+        podem não se aplicar a você.
+      </Typography>
+      <Typography paragraph>
+        Os materiais exibidos no site da Genap podem incluir erros técnicos,
+        tipográficos ou fotográficos. Genap não garante que qualquer material em
+        seu site seja preciso, completo ou atual. Genap pode fazer alterações
+        nos materiais contidos em seu site a qualquer momento, sem aviso prévio.
+        No entanto, Genap não se compromete a atualizar os materiais.
+      </Typography>
+      <Typography paragraph>
+        O Genap não analisou todos os sites vinculados ao seu site e não é
+        responsável pelo conteúdo de nenhum site vinculado. A inclusão de
+        qualquer link não implica endosso por Genap do site. O uso de qualquer
+        site vinculado é por conta e risco do usuário.
+      </Typography>
+      <Typography paragraph>
+        O Genap pode revisar estes termos de serviço do site a qualquer momento,
+        sem aviso prévio. Ao usar este site, você concorda em ficar vinculado à
+        versão atual desses termos de serviço.
+      </Typography>
+      <Typography paragraph>
+        Estes termos e condições são regidos e interpretados de acordo com as
+        leis do Genap e você se submete irrevogavelmente à jurisdição exclusiva
+        dos tribunais naquele estado ou localidade.
+      </Typography>
+    </Box>
+  );
+};
+
+export default function PoliticasPage() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="Politicas Tabs">
+          <Tab label="Política de Privacidade" />
+          <Tab label="Termos de Serviço" />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <PoliticaPrivacidade />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <TermosServico />
+      </TabPanel>
+    </div>
+  );
+}
+
+const TabPanel = (props: any) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+};
